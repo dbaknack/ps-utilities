ae5764c7d753473eb6a5d32f7ec0c89cb2c206c5
lab admin <dbaknack@gmail.com>
2025-04-29 16:02:44 -0600

    added example for setup

---
+++ examples/setup-up.ps1
@@ -0,0 +1,7 @@
+import-module .\ps-modules\ps-utilities
+
+# validate the commands available with this module
+get-command -module ps-utilities
+
+# optional: remove the module when no longer needed
+remove-module -name ps-utilities
